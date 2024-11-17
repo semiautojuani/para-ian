a01bbe0093a308c5d566edc406805b26c8430c0b
Juan I. Ventimiglia <167589242+semiautojuani@users.noreply.github.com>
2024-11-16 21:19:26 -0300

    Add files via upload

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,6 +1,7 @@
 let objJuego;
 let instancia = "instrucciones";
 let fondo, pj, villano, aliado, proyectil, intro;
+let cancionFondo;
 
 function preload(){
  fondo = loadImage ('data/0.png');
@@ -9,12 +10,15 @@ function preload(){
  aliado = loadImage('data/3.png');
  proyectil = loadImage ('data/4.png');
  intro = loadImage ('data/5.png');
+ 
+ cancionFondo = loadSound('data/sonido.mp3');
 }
 
 function setup() {
   createCanvas(640, 480);
   objJuego = new juego();
   
+  cancionFondo.setVolume(0.05);
 }
 
 function draw() {
@@ -26,6 +30,7 @@ function draw() {
    } else if (instancia === "créditos"){
      creditos();
    }
+  
 }
 
 function instrucciones(){
@@ -58,12 +63,18 @@ function keyPressed() {
   if (keyCode === 32) {
     if (instancia === "instrucciones") {
       instancia = "juego";
+      cancionFondo.loop();
     } else if (instancia === "juego") {
       instancia = "créditos";
     } else if (instancia === "créditos"){
       instancia = "instrucciones"
+      cancionFondo.stop();
+      cancionFondo.play();
     }
   }
+  
+
+  
   if (instancia === "juego"){
   objJuego.teclaPresionada(keyCode);
  }
